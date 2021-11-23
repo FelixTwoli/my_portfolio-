7c1ab717998dfdb6825f0671e3bbda34fc122452
FelixTwoli <felixtwoli21@gmail.com>
2021-11-23 11:05:44 +0300

    add js file

---
+++ js/index.js
@@ -0,0 +1,19 @@
+var age = 21;
+const felo = 23 / 05 / 2000;
+let name = "Twoli";
+console.log("age: + 21");
+console.log("date of birth: felo");
+
+const navbartoggler = document.getElementById(navbar - toggler);
+const collapse= document.querySelector(.navbar-collapse)
+
+console.log(navbartoggler);
+navbarToggker.addEventListener("click", function(){
+toggle();
+})
+
+
+function toggle() {
+         collapse.style.display = "flex";
+         collapse.style.flexDirection= "column";
+}
